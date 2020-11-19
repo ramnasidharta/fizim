3ea24b88e9a74421afac634e6f80601408607800
Ramna Sidharta <ramna.sidharta@leafagriculture.com.br>
2020-11-18 23:32:39 -0300

    Remove BalanceServiceImpl

--- fizz/src/main/java/com/modsim/fizz/service/BalanceService.java
+++ fizz/src/main/java/com/modsim/fizz/service/BalanceService.java
@@ -1,42 +1,53 @@
 package com.modsim.fizz.service;
 
+import com.modsim.fizz.domain.Balance;
+import com.modsim.fizz.repository.BalanceRepository;
 import com.modsim.fizz.service.dto.BalanceDTO;
+import com.modsim.fizz.service.mapper.BalanceMapper;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Service Interface for managing {@link com.modsim.fizz.domain.Balance}.
+ * Service for managing {@link com.modsim.fizz.domain.Balance}.
  */
-public interface BalanceService {
-    /**
-     * Save a balance.
-     *
-     * @param balanceDTO the entity to save.
-     * @return the persisted entity.
-     */
-    BalanceDTO save(BalanceDTO balanceDTO);
+@Service
+@Transactional
+@Slf4j
+public class BalanceService {
+    private final BalanceRepository balanceRepository;
 
-    /**
-     * Get all the balances.
-     *
-     * @param pageable the pagination information.
-     * @return the list of entities.
-     */
-    Page<BalanceDTO> findAll(Pageable pageable);
+    private final BalanceMapper balanceMapper;
 
-    /**
-     * Get the "id" balance.
-     *
-     * @param id the id of the entity.
-     * @return the entity.
-     */
-    Optional<BalanceDTO> findOne(Long id);
+    public BalanceService(BalanceRepository balanceRepository, BalanceMapper balanceMapper) {
+        this.balanceRepository = balanceRepository;
+        this.balanceMapper = balanceMapper;
+    }
 
-    /**
-     * Delete the "id" balance.
-     *
-     * @param id the id of the entity.
-     */
-    void delete(Long id);
+    public BalanceDTO save(BalanceDTO balanceDTO) {
+        log.debug("Request to save Balance : {}", balanceDTO);
+        Balance balance = balanceMapper.toEntity(balanceDTO);
+        balance = balanceRepository.save(balance);
+        return balanceMapper.toDto(balance);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<BalanceDTO> findAll(Pageable pageable) {
+        log.debug("Request to get all Balances");
+        return balanceRepository.findAll(pageable).map(balanceMapper::toDto);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<BalanceDTO> findOne(Long id) {
+        log.debug("Request to get Balance : {}", id);
+        return balanceRepository.findById(id).map(balanceMapper::toDto);
+    }
+
+    public void delete(Long id) {
+        log.debug("Request to delete Balance : {}", id);
+        balanceRepository.deleteById(id);
+    }
 }
