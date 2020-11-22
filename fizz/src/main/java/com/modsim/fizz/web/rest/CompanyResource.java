f7faa4530814d0299eaf33d372926fee53bb917e
Ramna Sidharta <ramna.sidharta@leafagriculture.com.br>
2020-11-22 16:30:50 -0300

    Fix endpoint of CompanyResource

--- fizz/src/main/java/com/modsim/fizz/web/rest/CompanyResource.java
+++ fizz/src/main/java/com/modsim/fizz/web/rest/CompanyResource.java
@@ -2,29 +2,26 @@ package com.modsim.fizz.web.rest;
 
 import com.modsim.fizz.service.CompanyService;
 import com.modsim.fizz.service.dto.CompanyDTO;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/companies/")
+@RequestMapping("/api/companies/")
 @Slf4j
 public class CompanyResource {
-    private CompanyService service;
+    private final CompanyService service;
 
     public CompanyResource(CompanyService service) {
         this.service = service;
     }
 
     @GetMapping
-    public ResponseEntity<Page<CompanyDTO>> findAll(@RequestBody CompanyDTO companyDTO, Pageable pageable) {
+    public ResponseEntity<Page<CompanyDTO>> findAll(CompanyDTO companyDTO, Pageable pageable) {
         log.info("REST request GET /companies with body {} and pagination {}", companyDTO, pageable);
         Page<CompanyDTO> result = service.findAllWithExample(companyDTO, pageable);
         return ResponseEntity.ok(result);
