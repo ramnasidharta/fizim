d38fde06457bfdfe0bfdd7dca761c31af318174d
Ramna Sidharta <ramna.sidharta@leafagriculture.com.br>
2020-11-22 14:22:18 -0300

    Fix name of CompanyDTO attributes

--- fizz/src/main/java/com/modsim/fizz/service/dto/CompanyDTO.java
+++ fizz/src/main/java/com/modsim/fizz/service/dto/CompanyDTO.java
@@ -1,7 +1,9 @@
 package com.modsim.fizz.service.dto;
 
+import com.modsim.fizz.domain.Company;
 import java.time.LocalDate;
 import lombok.Data;
+import org.springframework.beans.BeanUtils;
 
 /**
  * A DTO for the {@link com.modsim.fizz.domain.Company} entity.
@@ -10,17 +12,29 @@ import lombok.Data;
 public class CompanyDTO {
     private Integer cvmCode;
     private String cnpj;
-    private String social_denomination;
-    private String comercial_denomination;
-    private LocalDate register_date;
-    private LocalDate constitution_date;
-    private LocalDate cancellation_date;
-    private String cancellation_reason;
+    private String socialDenomination;
+    private String commercialDenomination;
+    private LocalDate registerDate;
+    private LocalDate constitutionDate;
+    private LocalDate cancellationDate;
+    private String cancellationReason;
     private String situation;
-    private LocalDate situation_start_date;
+    private LocalDate situationStartDate;
     private String sector;
     private String market;
     private String category;
-    private String cnpj_auditor;
+    private String cnpjAuditor;
     private String auditor;
+
+    public Company toEntity() {
+        Company entity = new Company();
+        BeanUtils.copyProperties(this, entity);
+        return entity;
+    }
+
+    public static CompanyDTO fromEntity(Company entity) {
+        CompanyDTO dto = new CompanyDTO();
+        BeanUtils.copyProperties(entity, dto);
+        return dto;
+    }
 }
