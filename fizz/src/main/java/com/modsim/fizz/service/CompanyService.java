c9aba05a31c3a1bf667f1193455fc3f3f70ba321
Ramna Sidharta <ramna.sidharta@leafagriculture.com.br>
2020-11-22 14:22:42 -0300

    Replace mapper by methods in DTO

--- fizz/src/main/java/com/modsim/fizz/service/CompanyService.java
+++ fizz/src/main/java/com/modsim/fizz/service/CompanyService.java
@@ -3,9 +3,9 @@ package com.modsim.fizz.service;
 import com.modsim.fizz.domain.Company;
 import com.modsim.fizz.repository.CompanyRepository;
 import com.modsim.fizz.service.dto.CompanyDTO;
-import com.modsim.fizz.service.mapper.CompanyMapper;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,30 +16,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class CompanyService {
     private final CompanyRepository repository;
-    private final CompanyMapper companyMapper;
 
-    public CompanyService(CompanyRepository repository, CompanyMapper mapper) {
+    public CompanyService(CompanyRepository repository) {
         this.repository = repository;
-        this.companyMapper = mapper;
     }
 
     public CompanyDTO save(CompanyDTO companyDTO) {
         log.debug("Request to save Balance : {}", companyDTO);
-        Company company = companyMapper.toEntity(companyDTO);
+        Company company = companyDTO.toEntity();
         company = repository.save(company);
-        return companyMapper.toDto(company);
+        return CompanyDTO.fromEntity(company);
     }
 
     @Transactional(readOnly = true)
-    public Page<CompanyDTO> findAll(Pageable pageable) {
+    public Page<CompanyDTO> findAllWithExample(CompanyDTO companyExample, Pageable pageable) {
         log.debug("Request to get all Companies");
-        return repository.findAll(pageable).map(companyMapper::toDto);
+        Example<Company> entityExample = Example.of(companyExample.toEntity());
+        return repository.findAll(entityExample, pageable).map(CompanyDTO::fromEntity);
     }
 
     @Transactional(readOnly = true)
     public Optional<CompanyDTO> findOne(Integer id) {
         log.debug("Request to get Company : {}", id);
-        return repository.findById(id).map(companyMapper::toDto);
+        return repository.findById(id).map(CompanyDTO::fromEntity);
     }
 
     public void delete(Integer id) {
