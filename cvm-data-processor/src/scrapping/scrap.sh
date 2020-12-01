ec2d24f994c83eeab1abdab289a3b5c87bfb0b4e
Ramna Sidharta <ramna.sidharta@leafagriculture.com.br>
2020-12-01 02:23:57 -0300

    Clean src/scrapping

--- cvm-data-processor/src/scrapping/scrap.sh
+++ cvm-data-processor/src/scrapping/scrap.sh
@@ -1,20 +1,20 @@
 
 # Run Parsehub scrapping project and get the token of the run
-run_token=$(curl -s -X POST https://www.parsehub.com/api/v2/projects/tKvGjvJ33SZ4/run -d api_key=tQY3DqCuXh2Q | jq -r .run_token)
+run_token=$(curl -s -X POST https://www.parsehub.com/api/v2/projects/tKvGjvJ33SZ4/run -d api_key=$API_KEY | jq -r .run_token)
 echo "Run token: " $run_token
 
 # Wait until the run is completed
-run_status=$(curl -s -X GET "https://www.parsehub.com/api/v2/runs/${run_token}?api_key=tQY3DqCuXh2Q" | jq -r .status)
+run_status=$(curl -s -X GET "https://www.parsehub.com/api/v2/runs/${run_token}?api_key=$API_KEY" | jq -r .status)
 echo "Run status: " $run_status
 while [ $run_status != complete ]; do
   echo "Wait..."
   sleep 1
-  run_status=$(curl -s -X GET "https://www.parsehub.com/api/v2/runs/${run_token}?api_key=tQY3DqCuXh2Q" | jq -r .status)
+  run_status=$(curl -s -X GET "https://www.parsehub.com/api/v2/runs/${run_token}?api_key=$API_KEY" | jq -r .status)
   echo "Run status: " $run_status
 done
 
 sleep 2
 
 echo "Downloading data"
-curl -X GET "https://www.parsehub.com/api/v2/runs/${run_token}/data?api_key=tQY3DqCuXh2Q" --output b3_companies_scrapped.gz
+curl -X GET "https://www.parsehub.com/api/v2/runs/${run_token}/data?api_key=$API_KEY" --output b3_companies_scrapped.gz
 
