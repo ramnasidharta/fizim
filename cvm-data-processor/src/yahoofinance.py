cb9d65e06029019f040bc9ee67dde69a6667fe30
Ramna Sidharta <ramna.sidharta@leafagriculture.com.br>
2020-12-01 02:24:47 -0300

    Update yahoofinance to work new errors around

--- cvm-data-processor/src/yahoofinance.py
+++ cvm-data-processor/src/yahoofinance.py
@@ -1,5 +1,15 @@
+import sys
+import logging
+
 import yfinance
-from src import utils
+
+import utils
+
+
+LOG = logging.getLogger('yahoofinance')
+LOG.setLevel(logging.DEBUG)
+stdout_handler = logging.StreamHandler(sys.stdout)
+LOG.addHandler(stdout_handler)
 
 
 def current_price(companies: dict):
@@ -9,18 +19,18 @@ def current_price(companies: dict):
     for company_id in companies:
         company = companies[company_id]
 
-        print('Obtaining data from', company['name'], f'(code={company["codes"]})')
+        LOG.info('Obtaining data from %s %s', company['name'], f'(code={company["codes"]})')
 
         try:
             # TODO: remember to remove '.SA' if the source file already
             # records codes with it
             ticker_info = yfinance.Ticker(company['codes'] + '.SA').info
         except Exception as e:
-            print(e)
+            LOG.error(e)
             continue
 
         previous_close = ticker_info['previousClose']
-        print('Price =', previous_close)
+        LOG.info('Price = %s', previous_close)
         current_price_list.append(previous_close)
 
         i = i + 1
@@ -36,26 +46,30 @@ def current_price():
     # TODO: remember to remove '.SA' if the source file already
     # records codes with it
     company_codes = list(map(lambda c: companies[c]['codes'] + '.SA', companies))
-    company_codes_str = ' '.join(company_codes[:100])
+    company_codes_str = ' '.join(company_codes)
 
     tickers = yfinance.Tickers(company_codes_str).tickers
 
     current_prices = {}
     i = 0  # to iterate over company_codes
     for ticker in tickers:
-        print('Obtaining data from', company_codes[i])
+        LOG.info('Obtaining data from %s', company_codes[i])
 
         try:
             ticker_current_price = ticker.info['previousClose']
-            print('Price:', ticker_current_price)
+            LOG.info('Price: %s', ticker_current_price)
         except KeyError as e:
             if e.args[0] == 'regularMarketOpen':
-                print('No regularMarketOpen found, skipping it.')
+                LOG.error('No regularMarketOpen found, skipping it.')
                 continue
+        except ValueError as e:
+            LOG.error(e)
+            continue
+
 
         current_prices[company_codes[i]] = ticker_current_price
         i += 1
 
-    print('Number of obtained last close price:', len(current_prices))
-    print(current_prices)
+    LOG.info('Number of obtained last close price: %d', len(current_prices))
+    LOG.info(current_prices)
     return current_prices
